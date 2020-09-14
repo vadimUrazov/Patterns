432fc37c7c153739a6453e78a104d5f92340ccfc
VadimUrazov <vadim.urazov@gmail.com>
2020-09-14 11:10:06 +0600

    Method Factory

---
+++ MethodFactory/src/main/java/org/example/Isort.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.List;
+
+public interface Isort {
+    void sortNatural();
+    void sortToStrLen();
+    void sortHash();
+}
---
+++ MethodFactory/src/main/java/org/example/SortedNumber.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortedNumber implements Isort{
+    private List<Integer> list=new ArrayList<>();
+    public SortedNumber(Integer...a) {
+        for (int i = 0; i < a.length; i++) {
+            list.add(a[i]);
+        }
+    }
+
+    @Override
+    public void sortNatural() {
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list.toString());
+    }
+
+    @Override
+    public void sortToStrLen() {
+
+    }
+
+    @Override
+    public void sortHash() {
+
+    }
+
+}
